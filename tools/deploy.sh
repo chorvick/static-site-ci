3d1898229a5092e8f87448b5fc0d5a77b09ecf86
Justin Rice <jsrice7391@gmail.com>
2022-06-23 18:41:02 -0400

    Added the catches for the awscli

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,11 +1,25 @@
 #! /bin/bash
 
 # Add a step that checks for a build folder and tell the user.
+if [ -d "static-site-lesson/build" ]; then
+  echo "Build folder located. Continuing..."
+else
+  echo "Build folder not set."
+  exit 1
+fi
+
 
 # Check for the AWS command
-
-# Install if it is not there
+if [ -x "$(command -v aws)" ]; then
+  echo "AWS command found. Continuing..."
+else
+  echo "AWS command not found. Installing AWSCLI"
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+  exit 1
+fi
 
 # Use the AWS command to upload the build to S3
+aws s3 ls
 
-echo "Hello world"
